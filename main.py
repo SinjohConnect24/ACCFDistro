63da5f07b2be847f75bec64c0ee7b1c75863745e
Sophia <77129415+sophia-angel@users.noreply.github.com>
2024-03-01 13:03:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import sys
 # First, we (kind of) efficiently perform all the sanity checks
 # and config passes.
 if data.PROD_MODE is True:
-    print("[*] Running in production mode... Hello zomka, your server is wishing you well.")
+    print("[*] Running in production mode... Hello, your server is wishing you well.")
     mux.runner(data.WEBHOOK, data.REGION_LETTER, data.WEBHOOK_PREFIX,
                data.PROD_WHOOK, data.PROD_PUSH,
                data.PROD_DIR, data.REGION_CODE)
 elif data.TEST_MODE is True:
-    print("[*] Running in test mode... Hello zomka, your server is wishing you well.")
+    print("[*] Running in test mode... Hello, your server is wishing you well.")
     mux.runner(data.WEBHOOK, data.REGION_LETTER, data.WEBHOOK_PREFIX,
                data.TEST_WHOOK, data.TEST_PUSH,
                data.TEST_DIR, data.REGION_CODE)
